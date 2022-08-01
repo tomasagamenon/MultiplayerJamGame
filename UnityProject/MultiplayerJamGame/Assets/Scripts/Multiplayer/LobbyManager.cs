39c4c8f6117225ddfc87025fb52a83cc12731da4
tomasagamenon <tomas.agamenon@gmail.com>
2022-08-01 12:12:50 -0300

    MultiplayerThings

---
+++ UnityProject/MultiplayerJamGame/Assets/Scripts/Multiplayer/LobbyManager.cs
@@ -0,0 +1,137 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    public TMP_InputField roomInputField;
+    public GameObject lobbyPanel;
+    public GameObject roomPanel;
+    public TMP_Text roomName;
+
+    public RoomItem roomItemPrefab;
+    private List<RoomItem> _roomItemsList = new List<RoomItem>();
+    public Transform contentObject;
+
+    public float timeBetweenUpdates;
+    private float _nextUpdateTime;
+
+    public List<PlayerItem> playerItemsList = new List<PlayerItem>();
+    public PlayerItem playerItemPrefab;
+    public Transform playerItemParent;
+
+    public GameObject playButton;
+
+    private void Start()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public void OnClick_Create()
+    {
+        if (roomInputField.text.Length >= 1)
+        {
+            PhotonNetwork.CreateRoom(roomInputField.text, new RoomOptions() { MaxPlayers = 2, BroadcastPropsChangeToAll = true});
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        lobbyPanel.SetActive(false);
+        roomPanel.SetActive(true);
+        roomName.text = "Room Name: " + PhotonNetwork.CurrentRoom.Name;
+        UpdatePlayerList();
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        if (Time.time >= _nextUpdateTime)
+        {
+            UpdateRoomList(roomList);
+            _nextUpdateTime = Time.time + timeBetweenUpdates;
+        }
+    }
+
+    private void UpdateRoomList(List<RoomInfo> list)
+    {
+        foreach (RoomItem item in _roomItemsList)
+            Destroy(item.gameObject);
+        _roomItemsList.Clear();
+        foreach (RoomInfo room in list)
+        {
+            RoomItem newRoom = Instantiate(roomItemPrefab, contentObject);
+            newRoom.SetRoomName(room.Name);
+            _roomItemsList.Add(newRoom);
+        }
+    }
+
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void OnClick_LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        lobbyPanel.SetActive(true);
+        roomPanel.SetActive(false);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    void UpdatePlayerList()
+    {
+        foreach (PlayerItem item in playerItemsList)
+            Destroy(item.gameObject);
+        playerItemsList.Clear();
+
+        if (PhotonNetwork.CurrentRoom == null)
+            return;
+
+        foreach (KeyValuePair<int, Player> player in PhotonNetwork.CurrentRoom.Players)
+        {
+            PlayerItem newPlayerItem = Instantiate(playerItemPrefab, playerItemParent);
+            newPlayerItem.SetPlayerInfo(player.Value);
+
+            if (player.Value==PhotonNetwork.LocalPlayer)
+            {
+                newPlayerItem.ApplyLocalChanges();
+            }
+
+            playerItemsList.Add(newPlayerItem);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        UpdatePlayerList();
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        UpdatePlayerList();
+    }
+
+    private void Update()
+    {
+        if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount >= 2)
+            playButton.SetActive(true);
+        else
+            playButton.SetActive(false);
+    }
+
+    public void OnClick_PlayButton()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
+}
