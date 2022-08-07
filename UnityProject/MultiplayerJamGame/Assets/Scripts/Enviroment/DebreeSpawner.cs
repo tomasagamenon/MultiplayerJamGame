057a30de1fa3346ee4202232d7fb4bf5ea932676
tomasagamenon <tomas.agamenon@gmail.com>
2022-08-07 07:54:07 -0300

    aa

--- UnityProject/MultiplayerJamGame/Assets/Scripts/Enviroment/DebreeSpawner.cs
+++ UnityProject/MultiplayerJamGame/Assets/Scripts/Enviroment/DebreeSpawner.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun;
-using Photon.Realtime;
 
 public class DebreeSpawner : MonoBehaviour
 {
@@ -12,7 +10,6 @@ public class DebreeSpawner : MonoBehaviour
     private float spawnCooldown = 0f;
     public GameObject[] debrisPrefs;
     public float minDebreeSpeed, maxDebreeSpeed;
-    public PhotonView photonView;
 
     private void Update()
     {
@@ -21,19 +18,11 @@ public class DebreeSpawner : MonoBehaviour
             Vector2 position = new Vector2(Random.Range(point1.position.x, point2.position.x), 
                 Mathf.Lerp(point1.position.y, point2.position.y, 0.5f));
             float rotation = Random.Range(minRotation, maxRotation);
-            float speed = Random.Range(minDebreeSpeed, maxDebreeSpeed);
-            int random = Random.Range(0, debrisPrefs.Length);
-            photonView.RPC("RTC_DebreeSpawn", RpcTarget.All, new object[] { position, rotation, speed , random });
+            Debree debree = Instantiate(debrisPrefs[Random.Range(0, debrisPrefs.Length)], position, Quaternion.Euler(0, 0, rotation)).GetComponent<Debree>();
+            debree.speed = Random.Range(minDebreeSpeed, maxDebreeSpeed);
             spawnCooldown = Time.time + 1f / spawnRate;
         }
     }
-    [Photon.Pun.RPC]
-    private void RTC_DebreeSpawn(Vector2 position, float rotation, float speed, int random)
-    {
-        Debree debree = PhotonNetwork.Instantiate(debrisPrefs[random].name, position, Quaternion.Euler(0, 0, rotation)).GetComponent<Debree>();
-        debree.speed = speed;
-    }
-
     private void OnDrawGizmosSelected()
     {
         Vector3 a = new Vector3(Mathf.Cos(Mathf.Deg2Rad * minRotation), Mathf.Sin(Mathf.Deg2Rad * minRotation), 0);
